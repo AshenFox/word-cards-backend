45831c2174e6b23473542b103a464be0eddefc49
HoarFox <kavokinm@gmail.com>
2020-05-12 19:38:08 +0300

    Commit 19:38 12.05.2020

--- ref/constants.js
+++ ref/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  corsURL: "http://127.0.0.1:8080", //'' null "https://hoarfox.github.io"
+  corsURL: "https://hoarfox.github.io", //'' null  "http://127.0.0.1:8080"
 };
 
 module.exports = constants;
